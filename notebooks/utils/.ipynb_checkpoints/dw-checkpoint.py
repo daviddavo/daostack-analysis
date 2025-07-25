8a1a9c70feef33480afd06bed0a032fdd8c6454d
David Davó <david@ddavo.me>
2025-07-25 12:56:11 +0000

    Removed more binaries
    Signed-off-by: David Davó <david@ddavo.me>

--- notebooks/utils/.ipynb_checkpoints/dw-checkpoint.py
+++
@@ -1,7 +0,0 @@
-import datetime as dt
-
-from dao_analyzer.web.apps.daostack.data_access.daos.metric import srcs as DAOSTACK
-
-def get_date() -> dt.datetime:
-    with open(DAOSTACK.DATAWAREHOUSE / 'update_date.txt', 'r') as f:
-        return dt.datetime.fromisoformat(f.readline())
